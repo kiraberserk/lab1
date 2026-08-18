e42253190f330a8c99ddb8690ed9a053cd6c9434
Kira Berserk <juanc.castilloc@gmail.com>
2026-08-18 17:23:26 -0400

    Unused variable

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          foo="bar"
         >
           Lab1 - CI workflow
         </a>
