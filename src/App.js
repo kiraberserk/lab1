50ddf31a764e6bdd766d44e17fa1447aaeaca9c5
Kira Berserk <juanc.castilloc@gmail.com>
2026-08-18 12:05:41 -0400

    Title changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lab1 - CI workflow
         </a>
       </header>
     </div>
